08d54b1716ee8c802e7590b4712c0058bc2654df
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-06 12:59:04 +0300

    add homework006

--- Homework006/Program.cs
+++ Homework006/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int a = 7;
+
+if (a % 2 == 0)
+{
+Console.WriteLine("Число делится на 2 без остатка");
+}
+else
+{
+Console.WriteLine("Число делится на 2 с остатком");    
+}
