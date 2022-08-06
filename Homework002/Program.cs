18b35bb9e53d5e19c6d18cec0551318d29adf342
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-06 11:56:25 +0300

    add homeworks

---
+++ Homework002/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = -3;
+int b = -9;
+if (a < b)
+{ 
+    Console.Write("min = ");
+    Console.WriteLine(a);
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("min = ");
+    Console.WriteLine(b);
+    Console.Write("max = ");
+    Console.WriteLine(a);  
+}
