31c26f88501f6272948b8195562ebeca346e437d
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-07 01:26:20 +0300

    update

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,16 +1,15 @@
-﻿Console.WriteLine("Введите число");
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показыват все целые числа в промежутке от -N до N
+Console.WriteLine("Введите положительное число");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = -a;
 
 while (a > b)
-    
-if (b != a)
 {
     Console.Write(b);
     Console.Write(" ");
     b = (b + 1);
 }
-
 Console.Write(a);
 
 
