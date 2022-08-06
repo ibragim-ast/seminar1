20402e9d4c07bda7ecb765ecfefedded83e7d61b
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-06 12:25:16 +0300

    add homework004

--- Homework004/Program.cs
+++ Homework004/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 22;
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max); 
