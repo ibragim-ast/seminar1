9991448375fd4398ed213bd5e0bd40373f851858
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-06 22:40:58 +0300

    add Task003

---
+++ Task001/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат данного числа = ");
+Console.WriteLine(a * a);
---
+++ Task003/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.Write("Понедельник");
+}     
+else if (a == 2)
+{
+    Console.Write("Вторник");
+}
+else if (a == 3)
+{    
+    Console.Write("Среда");
+}
+else if (a == 4)
+{ 
+    Console.Write("Четверг");
+}
+else if (a == 5)
+{
+    Console.Write("Пятница");
+}
+else if (a == 6)
+{
+    Console.Write("Суббота");
+}
+else if (a == 7)
+{
+    Console.Write("Воскресенье");
+}
